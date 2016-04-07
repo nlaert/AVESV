cda12be2924966a60dccaa93843917cb958070b5
nlaert <nlaert@gmail.com>
2016-04-07 20:27:13 +0100

    restored files location

---
+++ Jsonzai.Test/NumericTypesTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jsonzai.Reflect;
+
+namespace Jsonzai.Test
+{
+    [TestClass]
+    public class NumericTypesTest
+    {
+        
+        [TestMethod]
+        public void TestJsonfierIntValues()
+        {
+            int src = 10;
+            string actual = Jsonfier.ToJson(src);
+            Assert.AreEqual(src.ToString(), actual);
+        }
+
+        [TestMethod]
+        public void TestJsonfierDoubleValue()
+        {
+            double src = 100000.2;
+            string actual = Jsonfier.ToJson(src);
+            Assert.AreEqual("100000.2", actual);
+        }
+
+        [TestMethod]
+        public void TestJsonfierFloatValue()
+        {
+            float src = 100000.2F;
+            string actual = Jsonfier.ToJson(src);
+            Assert.AreEqual("100000.2", actual);
+        }
+    }
+    
+}

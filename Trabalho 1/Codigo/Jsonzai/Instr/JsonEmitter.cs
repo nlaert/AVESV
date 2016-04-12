1df1db6fdd18401a7e4f0913268acba3801a1f83
jprisel <a35392@alunos.isel.pt>
2016-04-12 21:53:06 +0100

    Trabalho Fase 2

---
+++ Trabalho 1/Codigo/Jsonzai/Instr/JsonEmitter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jsonzai.Instr
+{
+    public class JsonEmitter
+    {
+        public static string AssemblyName = "JsonfyFor";
+
+    }
+}
---
+++ Trabalho 1/Codigo/Jsonzai/Instr/Jsoninstr.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Jsonzai.Instr
+{
+    class Jsoninstr
+    {
+        Dictionary<String, IJsonfier> dict = new Dictionary<string, IJsonfier>();
+
+        public static void ToJson(object obj)
+        {
+            LoadAssemblies();
+            IJsonfier jsonfier = FindObject(obj);
+            jsonfier.Jsonfy(obj);
+
+
+        }
+
+        private static IJsonfier FindObject(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void LoadAssemblies()
+        {
+            
+        }
+    }
+}
